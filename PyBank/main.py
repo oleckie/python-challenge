d2e822f0020ac500152dfaa1f536f470ce1602da
Orlando Leckie <101525457+oleckie@users.noreply.github.com>
2022-04-10 08:43:36 -0500

    All information completed with labels. Missing output.

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,15 +16,17 @@ proffitloss = []
 #link data
 budgetdata = os.path.join("Resources", "budget_data.csv")
 
+#title
 print("Financial Analysis")
 print("------------------")
 
+#where to look for data for everytjing below
 with open(budgetdata) as csvfile:
     csvreader=csv.reader(csvfile)
     next(csvreader, None)    
     
 
-#total months with heading
+#total months
     for row in csvreader:
         totalmonths+=1
     
@@ -49,7 +51,7 @@ with open(budgetdata) as csvfile:
             lowestmonth = row[0]
             lowestrev = monthlychange
 
-    
+#results with labels    
     print("Total Months:", totalmonths)
     print(f"Total Revenue: ${totalrev}")
     print(f"Average Change: ${averagechange}")
