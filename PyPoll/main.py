bb6af9cbb865628eafb39c8d22a58789d473ce05
Orlando Leckie <101525457+oleckie@users.noreply.github.com>
2022-04-12 15:35:00 -0500

    Final product.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -37,7 +37,14 @@ with open(polls) as csvfile:
     print("Total Votes:", len(candidatelist))
     print("-------------------------")
     x = len(candidatelist)
-    
+resultstxt = os.path.join("Analysis", "Results.txt")
+with open(resultstxt, 'w') as outfile:  
+
+#write to output (export)
+    outfile.write("Election Results\n")
+    outfile.write("-------------------------\n")
+    outfile.write(f"Total Votes: {len(candidatelist)}\n")
+    outfile.write("-------------------------\n")  
     for person in candidates:
         personvotes = 0
             
@@ -48,19 +55,34 @@ with open(polls) as csvfile:
 #new line (percent votes)
         candperc = (personvotes/x)
       
-        print(f'{person}, {personvotes}, {candperc:.3%}')
+        print(f'{person}: {candperc:.3%} ({personvotes})')
+
+#write to output (export)
+        outfile.write(f"{person}: {candperc:.3%} ({personvotes})\n")
+    outfile.write("-------------------------\n")
+    outfile.write("Winner:  Khan\n")
+    outfile.write("-------------------------\n")
+
     print("-------------------------")
     print("Winner:  Khan")
     print("-------------------------")
 
+
+    
+# print(candidates)
+# #print(candidatelist)
+# print(votes)
+# print(person)
+# print(personvotes)
+# print(candperc)
 #export results
-resultstxt = os.path.join("Analysis", "Results.txt")
-with open(resultstxt, 'w') as outfile:
-        outfile.write("Election Results\n")
-        outfile.write("-------------------------\n")
-        outfile.write(f"Total Votes:, {len(candidatelist)}\n")
-        outfile.write("-------------------------\n")
-        outfile.write(f"{person}, {personvotes}, {candperc:.3%}\n")
-        outfile.write("-------------------------\n")
-        outfile.write("Winner:  Khan\n")
-        outfile.write("-------------------------\n")
+# resultstxt = os.path.join("Analysis", "Results.txt")
+#with open(resultstxt, 'w') as outfile:
+        # outfile.write("Election Results\n")
+        # outfile.write("-------------------------\n")
+        # outfile.write(f"Total Votes:, {len(candidatelist)}\n")
+        # outfile.write("-------------------------\n")
+        # outfile.write(f"{person}, {personvotes}, {candperc:.3%}\n")
+        # outfile.write("-------------------------\n")
+        # outfile.write("Winner:  Khan\n")
+        # outfile.write("-------------------------\n")
